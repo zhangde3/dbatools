836df902e8c240bd141d6496672ee981b76d75c5
dennis <dennis@devops.localdomain>
2018-03-13 23:30:50 +0800

    update functional test

--- functional_test.py
+++ functional_test.py
@@ -1,36 +1,32 @@
 from selenium import webdriver
+import unittest
 
-browser = webdriver.Firefox()
+class NewDBATest(unittest.TestCase):
+    
+    def setUp(self):
+        self.browser = webdriver.Firefox()
+        self.browser.implicitly_wait(3)
 
-# login dbatools app 
+    def tearDown(self):
+        self.browser.quit()
 
+    def test_can_list_sessions_and_kill(self):
+        self.browser.get('http://127.0.0.1:8000')
+        # see keyword dbatools in browser title
+        self.assertIn('DBATools',self.browser.title)
+        # see "Oracle, MySQL, MongoDB" tabs at the navigate bar
+        # click "Oracle" tab will jump to url: "http://127.0.0.1:8000/oraclemgmt" 
+        # and list all oracle services, one of them is "csqa4"
+        # click "csqa4" at services list will jump to "url: http://127.0.0.1:8000/oraclemgmt/csqa4"
+        # and see connect information "connected: 10.222.90.88:1521/csqa4" at top of page, "disconnect" button at besides
+        # and see text "actions" at below, and action list, one of the action list is "query sessions"
+        # click link "query sessions" will jump to url: "http://127.0.0.1:8000/oraclemgmt/csqa4/sessions"
+        # and list a table of current sessions, see username in one of sessions is "super"
+        # and see "back to actions" button at both top and botton of page
+        # click the button "back to actions", will jump to url: "http://127.0.0.1:8000/oraclemgmt/csqa4"
+        # click the button "disconnect" will jump to url: "http://127.0.0.1:8000/oraclemgmt"
+        self.fail('Finish the test!')
 
-browser.get('http://127.0.0.1:8000')
+if __name__ == '__main__':
+    unittest.main()
 
-# see keyword dbatools in browser title
-
-assert 'DBATools' in browser.title
-
-# see "Oracle, MySQL, MongoDB" tabs at the navigate bar
-
-# click "Oracle" tab will jump to url: "http://127.0.0.1:8000/oraclemgmt" 
-
-# and list all oracle services, one of them is "csqa4"
-
-# click "csqa4" at services list will jump to "url: http://127.0.0.1:8000/oraclemgmt/csqa4"
-
-# and see connect information "connected: 10.222.90.88:1521/csqa4" at top of page, "disconnect" button at besides
-
-# and see text "actions" at below, and action list, one of the action list is "query sessions"
-
-# click link "query sessions" will jump to url: "http://127.0.0.1:8000/oraclemgmt/csqa4/sessions"
-
-# and list a table of current sessions, see username in one of sessions is "super"
-
-# and see "back to actions" button at both top and botton of page
-
-# click the button "back to actions", will jump to url: "http://127.0.0.1:8000/oraclemgmt/csqa4"
-
-# click the button "disconnect" will jump to url: "http://127.0.0.1:8000/oraclemgmt"
-
-browser.quit()
